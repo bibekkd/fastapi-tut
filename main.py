d911cff41d065d9afc052fecce17e63e18fc20c1
bibekkd <deybibek58@gmail.com>
2025-11-18 21:54:29 +0530

    Add root endpoint to return welcome message for the FastAPI Blog Application

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ app.include_router(blog_router)
 app.include_router(user_router)
 app.include_router(auth_router)
 
+app.get("/")
+def read_root():
+    return {"message": "Welcome to the FastAPI Blog Application!"}
 
 # if __name__ == "__main__":
 #     uvicorn.run(app, host="127.0.0.1", port=9000)
